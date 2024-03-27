abaf45922d585fbb54a6f7412299644b83e9a11d
Felix Habib <fhabib@seek.com.au>
2024-03-27 15:43:05 +1100

    Exercise 2: Mobile header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import styled from "styled-components/macro";
-
-import { QUERIES } from "../../constants";
-
 import Header from "../Header";
 import ShoeIndex from "../ShoeIndex";
 
